258d196ad8224156cb670bf1ae1514c002dcdbb8
Meflahi Abdelkader <abdelkadermeflahi40@gmail.com>
2026-04-09 22:06:42 +0100

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,13 +1,9 @@
-from .unary_logic import *
-from .causal_graph import *
-from .symbolic_patterns import *
-from .explainability_engine import *
+from .memory import (
+    KnowledgeBase, 
+    SemanticStore, 
+    ContextManager, 
+    BetaRootMemory,
+    create_memory_system
+)
 
-__all__ = [
-    'UnaryLogicEngine', 'UnaryState',
-    'CausalGraphBuilder',
-    'SymbolicPatternsEngine',
-    'ExplainabilityEngine',
-    'create_engine', 'create_causal_builder',
-    'create_symbolic_engine', 'create_explainability_engine'
-]
+__all__ += ['KnowledgeBase', 'BetaRootMemory', 'create_memory_system']
