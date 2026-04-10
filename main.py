4f2e13c99592a663c0fde29763bc33d9c077cdb2
Meflahi Abdelkader <abdelkadermeflahi40@gmail.com>
2026-04-10 18:42:22 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from core.scientific_method import ScientificInference
+
+# إنشاء العقل العلمي للنظام
+beta_root_ai = ScientificInference()
+
+# محاكاة تجربة علمية (سلسلة من الملاحظات)
+# لاحظ وجود "تناقضات" (أصفار وسط الواحدات)
+water_exp_data = [1, 1, 1, 0, 1, 1, 0, 1] 
+
+# إطلاق دورة البحث
+beta_root_ai.research_cycle("water_boiling", water_exp_data)
